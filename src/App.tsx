bb2777bd43ab261b91790dfa72bcb4e838eabb32
Isaque Oliveira <zazac3179@gmail.com>
2024-11-17 11:45:25 -0300

    feat: Rotas adicionadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
+
 function App() {
   return (
-    <h1>App</h1>
+    <Routes>
+      <Route index element={ <Login /> } />
+      <Route path="*" element={ <h1>Not Found</h1> } />
+    </Routes>
   );
 }
 
