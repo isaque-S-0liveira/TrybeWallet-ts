4d5ea820ecf17fb692d4ebf62a5b6a3390cc8c3a
Isaque Oliveira <zazac3179@gmail.com>
2024-12-27 09:08:27 -0300

    feat: TableHeader component

---
+++ src/components/TableHeader/TableHeader.tsx
@@ -0,0 +1,18 @@
+function TableHeader() {
+  return (
+    <tr>
+      <th id="th-1">#</th>
+      <th>Descrição</th>
+      <th>Tag</th>
+      <th>Método de pagamento</th>
+      <th>Valor</th>
+      <th>Moeda</th>
+      <th>Câmbio utilizado</th>
+      <th>Valor convertido</th>
+      <th>Moeda de conversão</th>
+      <th>Editar/Excluir</th>
+    </tr>
+  );
+}
+
+export default TableHeader;
